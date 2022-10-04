957d7778b2aa0e185d4e91640373f2af0dd69efd
Clair-Hawkes <hawkesj7@gmail.com>
2022-10-04 15:48:24 -0700

    Post route for creating new Note with title and content

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ connect_db(app)
 
 @app.get('/api/notes')
 def get_all_notes():
-    """Return JSON list of notes
+    """Return JSON list of all notes
         - {'notes': [{id, title, created, content}, ...]}
     """
-    
+
     notes = Note.query.all()
     serialized = [n.serialize() for n in notes]
     return jsonify(notes=serialized)
@@ -34,20 +34,49 @@ def get_single_note(n_id):
     """Return JSON with info about single note
         - {'note': {id, title, created, content}}
     """
-    
+
     note = Note.query.get_or_404(n_id)
     serialized = note.serialize()
     return jsonify(note=serialized)
-    
+
+@app.get('/api/notes/<n_value>')
+def get_filtered_notes(n_value):
+    """Return JSON with info about all notes whose title contains the query
+    parameter value.
+        - {'notes': [{id, title, created, content}, ...]}
+    """
+
+    notes = Note.query.filter(Note.title.like(f'%{n_value}%'))
+    serialized = [n.serialize() for n in notes]
+    return jsonify(notes=serialized)
+
+# Post a Note route
+@app.post('/api/notes')
+def create_note():
+    """Create a new note instance in databse.
+    Return JSON with info about the created note.
+    Only creates 1 note at a time."""
+
+    new_note = Note(
+        title = request.json["title"],
+        content = request.json["content"],
+    )
+
+    db.session.add(new_note)
+    db.session.commit()
+
+    serialized = new_note.serialize()
+    return (jsonify(note=serialized),201)
+
+
 # Thinking space
 # ----
 # Use sqLite - done
 # Create a table /w sqLite - done
 # ----
-# Routes
-# Restful
-# Get 1 note
-# Get all notes
-# Get note(s) by filter
+# Restful Routes
+# Get 1 note - done
+# Get all notes - done
+# Get note(s) by filter - done
 # ----
 # API Insomnia Post & Delete a note
